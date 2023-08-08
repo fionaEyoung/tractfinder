7b5ab8ff58ff509a6f09ff2cc4ec7736bc2ce883
Fiona Young <fiona.young.15@ucl.ac.uk>
2023-08-08 20:17:46 +0100

    Overhaul of brain mask handling
    - transform and struct no longer mutex, as stuct can additionally be 
    provided for brain masking (for tumour deformation)
    - only flag for whether a brain mask needs to be made
    - brain mask always regridded to 1mm for tumour deformation, and to 
    struct space for betting
    - tumour mask regridded to match brain mask

--- bin/tractfinder.py
+++ bin/tractfinder.py
@@ -7,14 +7,11 @@ if sys.version_info <= (3, 4):
   # stick with deprecated imp for v3.3 for protection of my sanity
   # Note to self: script is already python2 incompatible anyways because of use of fstrings. Change this?
   import imp
-  print("Using imp. Python version: "+sys.version)
   fp, pathname, description = imp.find_module('tractfinder', [ libpath ])
   imp.load_module('tractfinder', fp, pathname, description)
 
 else:
   import importlib.util as imput
-
-  print("Using importlib. Python version: "+sys.version)
 
   MODULE_NAME = 'tractfinder'
   MODULE_PATH = os.path.join(libpath, MODULE_NAME, '__init__.py')
